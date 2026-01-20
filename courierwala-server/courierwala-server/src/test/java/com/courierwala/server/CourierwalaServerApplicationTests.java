19bbba0be3ff78c1f453c627e3191a1163aa2d1d
omkharmate <omkharmate85@gmail.com>
2026-01-20 13:28:02 +0530

    initialized spring-boot(4.0.1)  backend with maven bundler JAVA-21

---
+++ courierwala-server/courierwala-server/src/test/java/com/courierwala/server/CourierwalaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courierwala.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierwalaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
